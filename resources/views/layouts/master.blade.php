b7211a364e45eb759fc88c87e5545d3a0c6bad1d
Jamie Law <jamiepeterlaw@gmail.com>
2018-08-11 14:26:36 +0100

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>@yield('title')</title>
+
+        <style>
+            
+        </style>
+    </head>
+    <body>
+    	<div class="container">
+    		@yield('content')
+    	</div>
+        
+    </body>
+</html>
